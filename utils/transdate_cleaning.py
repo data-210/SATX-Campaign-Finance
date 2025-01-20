381c79390bff5c90f34631c985f671ef4a9fa929
Jack Turek <jackturek@jacks-mbp.lan>
2025-01-20 11:29:49 -0600

    Clean & process updated campaign finance data

---
+++ utils/transdate_cleaning.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import openpyxl
+import re
+
+# Paths to the single CSV and Excel files
+csv_file_path = '/Users/jackturek/Documents/Repos/SATX-Campaign-Finance/data/campaignfinance_01202024.csv'
+excel_file_path = '/Users/jackturek/Documents/Repos/SATX-Campaign-Finance/data/campaignfinance_01202024.xlsx'
+
+# Load data
+df_csv = pd.read_csv(csv_file_path)
+df_excel = pd.read_excel(excel_file_path)
+
+# Convert TransDate col to datetime
+df_csv['TransDate:'] = pd.to_datetime(df_csv['TransDate:'])
+df_excel['TransDate:'] = pd.to_datetime(df_excel['TransDate:'])
+
+# Filter for dates on or after 10/27/2024
+filtered_csv = df_csv[df_csv['TransDate:'] >= '10/27/24']
+filtered_excel = df_excel[df_excel['TransDate:'] >= '10/27/24']
+
+# Save filtered datasets
+filtered_csv.to_csv("campaignfinanceupdatejan25.csv", index=False)
+filtered_excel.to_excel("campaignfinanceupdatejan25.xlsx", index=False)
+
+
